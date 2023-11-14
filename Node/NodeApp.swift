0cf5552357af3d576d98886d031e213acb8c5b71
Daria <sypivos@icloud.com>
2023-11-14 21:16:30 +0400

    Initial Commit

---
+++ Node/NodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NodeApp.swift
+//  Node
+//
+//  Created by Дарья Петренко on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct NodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
